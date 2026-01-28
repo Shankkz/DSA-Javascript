5f2803081d32e97b0db4e96a00674484f6efe1ef
Shankz <127765412+Shankkz@users.noreply.github.com>
2026-01-28 11:35:53 +0530

    Time: 11 ms (69.04%) | Memory: 58.5 MB (80.20%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let freq = {};
+
+    for(let num of nums){
+        freq[num] = (freq[num] || 0)+ 1;
+    }
+
+    return Object.keys(freq)
+    .sort((a,b) => freq[b] - freq[a])
+    .slice(0,k)
+    .map(Number);
+};
