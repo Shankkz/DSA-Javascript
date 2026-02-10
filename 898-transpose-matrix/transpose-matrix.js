55113fb78261bc69d1b1a5d53c60e6e2debc5079
Shankz <127765412+Shankkz@users.noreply.github.com>
2026-02-10 09:48:15 +0530

    Time: 1 ms (70.08%) | Memory: 57.7 MB (69.53%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    const res = [];
+
+    for(let i=0;i<matrix[0].length; i++){
+        const temp = [];
+
+        for(let j=0;j<matrix.length;j++){
+            temp.push(matrix[j][i]);
+        }
+
+        res.push(temp);
+    }
+
+    return res;
+};
