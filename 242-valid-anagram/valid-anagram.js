eb0d01ee22f94c796d73cacc52b29be2d9253d72
Shankz <127765412+Shankkz@users.noreply.github.com>
2026-03-15 18:17:47 +0530

    Time: 9 ms (85.73%) | Memory: 55.6 MB (76.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,10 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split('').sort().join('');
-    t = t.split('').sort().join('');
-    if(s===t){
-        return true;
+    if(s.length !== t.length) return false;
+
+    let obj1 = {};
+    let obj2 = {};
+
+    for(let i=0;i<s.length;i++){
+        obj1[s[i]] = (obj1[s[i]] || 0) +1;
+        obj2[t[i]] = (obj2[t[i]] || 0) +1;
     }
-    return false;
+
+    for(const key in obj1){
+        if(obj1[key] !== obj2[key] ) return false;
+    }
+
+    return true;
 };
